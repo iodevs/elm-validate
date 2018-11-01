2b8a15b45a3a982dc2e7e4af3a1c1fb9376ec94c
Ondrej Tucek <ondrej.tucek@gmail.com>
2018-11-01 22:04:32 +0100

    Added Validation tests

---
+++ tests/ValidationTests.elm
@@ -0,0 +1,193 @@
+module ValidationTests exposing (suite)
+
+import Expect
+import Fuzz exposing (Fuzzer, bool, constant, float, int, list, oneOf, string)
+import Test exposing (..)
+import Validation exposing (..)
+import Validators exposing (..)
+
+
+suite : Test
+suite =
+    describe "is now verifying the function"
+        [ describe "rawValue, which"
+            [ fuzz string "should return raw value from Field" <|
+                \raw ->
+                    let
+                        fields =
+                            [ Field raw NotValidated
+                            , Field raw (Valid raw)
+                            , Field raw (Invalid "invalid")
+                            ]
+
+                        results =
+                            fields
+                                |> List.map (\f -> rawValue f == raw)
+                    in
+                    Expect.true "Expected True." (List.foldl (&&) True results)
+            ]
+        , describe "validity, which"
+            [ fuzz string "should return validity from Field" <|
+                \raw ->
+                    let
+                        fields =
+                            [ ( Field raw NotValidated, NotValidated )
+                            , ( Field raw (Valid raw), Valid raw )
+                            , ( Field raw (Invalid "invalid"), Invalid "invalid" )
+                            ]
+
+                        results =
+                            fields
+                                |> List.map (\( f, expected ) -> validity f == expected)
+                    in
+                    Expect.true "Expected True." (List.foldl (&&) True results)
+            ]
+        , describe "field, which"
+            [ fuzz2 float string "should return default settings of Field with `NotValidated` validity" <|
+                \rawFloat rawString ->
+                    let
+                        fields =
+                            ( ( field rawFloat, rawFloat, NotValidated )
+                            , ( field rawString, rawString, NotValidated )
+                            )
+
+                        ( rv1, rv2 ) =
+                            fields
+                                |> Tuple.mapFirst
+                                    (\( f, r, ex ) -> (rawValue f == r) && (validity f == ex))
+                                |> Tuple.mapSecond
+                                    (\( f, r, ex ) -> (rawValue f == r) && (validity f == ex))
+                    in
+                    Expect.true "Expected True." (List.foldl (&&) True [ rv1, rv2 ])
+            ]
+        , describe "preValidatedField, which"
+            [ fuzz2 float string "should return default settings of Field with `Valid a` validity" <|
+                \rawFloat rawString ->
+                    let
+                        fields =
+                            ( ( preValidatedField String.fromFloat rawFloat, String.fromFloat rawFloat, Valid rawFloat )
+                            , ( preValidatedField identity rawString, rawString, Valid rawString )
+                            )
+
+                        ( rv1, rv2 ) =
+                            fields
+                                |> Tuple.mapFirst
+                                    (\( f, r, ex ) -> (rawValue f == r) && (validity f == ex))
+                                |> Tuple.mapSecond
+                                    (\( f, r, ex ) -> (rawValue f == r) && (validity f == ex))
+                    in
+                    Expect.true "Expected True." (List.foldl (&&) True [ rv1, rv2 ])
+            ]
+        , describe "validate, which"
+            [ fuzz int "should validate Field with `Event`" <|
+                \val ->
+                    let
+                        raw =
+                            String.fromInt val
+
+                        expField =
+                            if 0 < val then
+                                Field raw (Valid val)
+
+                            else
+                                Field raw (Invalid "ERROR")
+
+                        validator =
+                            isPositiveInt "ERROR"
+
+                        eventVal =
+                            [ ( OnSubmit, Field raw NotValidated, expField )
+                            , ( OnSubmit, Field raw (Valid val), expField )
+                            , ( OnSubmit, Field raw (Invalid "ERROR2"), expField )
+                            , ( OnBlur, Field raw NotValidated, expField )
+                            , ( OnBlur, Field raw (Valid val), expField )
+                            , ( OnBlur, Field raw (Invalid "ERROR2"), expField )
+                            , ( OnChange raw, Field "10" NotValidated, Field raw NotValidated )
+                            , ( OnChange raw, Field "10" (Valid 10), expField )
+                            , ( OnChange raw, Field "10" (Invalid "ERROR2"), expField )
+                            , ( OnRelatedChange, Field raw NotValidated, Field raw NotValidated )
+                            , ( OnRelatedChange, Field raw (Valid val), expField )
+                            , ( OnRelatedChange, Field raw (Invalid "ERROR2"), expField )
+                            ]
+
+                        results =
+                            eventVal
+                                |> List.map
+                                    (\( event, field_, expected ) ->
+                                        validate event validator field_ == expected
+                                    )
+                    in
+                    Expect.true "Expected True." (List.foldl (&&) True results)
+            ]
+        , describe "applyValidity, which"
+            [ test "should validate a pair of `Validity a`" <|
+                \_ ->
+                    let
+                        applyVal =
+                            [ ( NotValidated, NotValidated, NotValidated )
+                            , ( NotValidated, Valid identity, NotValidated )
+                            , ( NotValidated, Invalid "invalid", NotValidated )
+                            , ( Valid "raw", NotValidated, NotValidated )
+                            , ( Valid "raw", Valid identity, Valid "raw" )
+                            , ( Valid "raw", Invalid "invalid", Invalid "invalid" )
+                            , ( Invalid "invalid", NotValidated, Invalid "invalid" )
+                            , ( Invalid "invalid", Valid identity, Invalid "invalid" )
+                            , ( Invalid "invalid", Invalid "invalid", Invalid "invalid" )
+                            ]
+
+                        results =
+                            applyVal
+                                |> List.map (\( v1, v2, expected ) -> applyValidity v1 v2 == expected)
+                    in
+                    Expect.true "Expected True." (List.foldl (&&) True results)
+            ]
+        , describe "composite, which"
+            [ fuzz (oneOf [ string, constant "", constant "test@test.com" ]) "should return validator result" <|
+                \str ->
+                    let
+                        result =
+                            composite (isNotEmpty "ERROR1") (isEmail "ERROR2") str
+                    in
+                    case result of
+                        Ok v ->
+                            Expect.equal v str
+
+                        Err err ->
+                            Expect.true "Expected True." (err == "ERROR1" || err == "ERROR2")
+            ]
+        , describe "extractError, which"
+            [ fuzz string "should return error message from Field" <|
+                \raw ->
+                    let
+                        fields =
+                            [ ( Field raw NotValidated, Nothing )
+                            , ( Field raw (Valid raw), Nothing )
+                            , ( Field raw (Invalid "invalid"), Just "invalid" )
+                            ]
+
+                        results =
+                            fields
+                                |> List.map (\( f, expected ) -> extractError f == expected)
+                    in
+                    Expect.true "Expected True." (List.foldl (&&) True results)
+            ]
+        , describe "optional, which"
+            [ fuzz (oneOf [ Fuzz.map String.fromInt int, constant "" ]) "should return result of Optional Field validation" <|
+                \raw ->
+                    let
+                        result =
+                            optional (isPositiveInt "ERROR") raw
+                    in
+                    case result of
+                        Ok val ->
+                            case val of
+                                Just v ->
+                                    Expect.true "Expected True." (String.fromInt v == raw)
+
+                                Nothing ->
+                                    Expect.pass
+
+                        Err err ->
+                            Expect.equal err "ERROR"
+            ]
+        ]
